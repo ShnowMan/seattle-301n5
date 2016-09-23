626d7218214773972c77d1f84cfa385abe1316d3
Jeff King <king.jeffey@gmail.com>
2016-09-22 18:47:33 -0700

    fixed small bug

--- class-02-jquery-and-the-dom/pair-assignment/starter-code/scripts/articleView.js
+++ class-02-jquery-and-the-dom/pair-assignment/starter-code/scripts/articleView.js
@@ -4,7 +4,7 @@ var articleView = {};
 
 articleView.toggleNavDisplay = function() {
   // TODO: if you want a challenge, write an event handler to toggle the display property of the nav menu.
-  // hint: create a 'hidden' class that can be applied to or removed from the nav ul 
+  // hint: create a 'hidden' class that can be applied to or removed from the nav ul
 };
 
 articleView.toggleNavDisplay();
