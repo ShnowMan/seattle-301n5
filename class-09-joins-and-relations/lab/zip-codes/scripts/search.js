736dbbcbc2d78b9d89375761eff04e18159408cb
Jeff King <king.jeffey@gmail.com>
2016-10-16 13:03:12 -0700

    got stats populating filter.

--- class-09-joins-and-relations/lab/zip-codes/scripts/search.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/search.js
@@ -2,6 +2,18 @@
 
   // TODO: Write the code to populate your filters, and enable the search queries here in search.js
   // TODO: You will also interact with the map.js file here
+  function mapFunc(){}
 
+  mapFunc.statesPop = function(){
+      webDB.execute([{
+        'sql': 'SELECT DISTINCT state FROM zips ORDER BY state'
+      }], function(results){
+        console.log(results);
+        results.forEach(function(result){
+          $('#state-select').append('<option>' + result.state + '</option>')
+        })
+      });
+  };
 
+  module.mapFunc = mapFunc;
 })(window)
