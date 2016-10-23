90e0f48c186dde0326aba65d04bf9aba21aba1e2
Trenton Kress <trenton.kress@gmail.com>
2016-10-23 11:11:56 -0700

    Set up the pages routes and display methods

--- class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
@@ -1,9 +1,11 @@
 (function(module) {
   var aboutController = {};
 
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+  // TODONE: Define a function that hides all main section elements, and then reveals just the #about section:
   aboutController.index = function() {
-
+    $('.tab-content').hide();
+    $('#about').show();
+    console.log('AHAA!');
   };
 
   module.aboutController = aboutController;
