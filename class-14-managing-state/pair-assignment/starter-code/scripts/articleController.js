8ee255e9043b091b82eb57fb46f07e3e202fc85b
Trenton Kress <trenton.kress@gmail.com>
2016-10-30 14:39:35 -0700

    Two of the comments finished

--- class-14-managing-state/pair-assignment/starter-code/scripts/articleController.js
+++ class-14-managing-state/pair-assignment/starter-code/scripts/articleController.js
@@ -8,6 +8,13 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /*
+   * This method grabs one article from the database with Article.findWhere and
+   * then sets the articles object to the selected article so it can be rendered
+   * to the view with the index function that gets called by next(). The article
+   * that is found in the database is based on the id argument passed in from
+   * the URL of the request.
+   */
   articlesController.loadById = function(ctx, next) {
     var articleData = function(article) {
       ctx.articles = article;
@@ -18,6 +25,10 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /*
+   * Does the same thing as above except it filters the articles to display by
+   * author id.
+   */
   articlesController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
